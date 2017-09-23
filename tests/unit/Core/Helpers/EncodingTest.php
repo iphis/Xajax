3fbace2e116b481445b04301fe08ade432dd6f59
roman JProof <12245927+JProof@users.noreply.github.com>
2017-09-23 15:54:23 +0200

    Init Codeception

---
+++ tests/unit/Core/Helpers/EncodingTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * PHP version php7
+ *
+ * @category
+ * @package            xajax-php-7
+ * @author             ${JProof}
+ * @copyright          ${copyright}
+ * @license            ${license}
+ * @link
+ * @see                ${docu}
+ * @since              23.09.2017
+ */
+
+declare(strict_types=1);
+
+namespace Xajax\Helpers;
+
+use PHPUnit\Framework\TestCase;
+
+class EncodingTest extends TestCase
+{
+}
---
+++ tests/unit/_bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * PHP version php7
+ *
+ * @category
+ * @package            xajax-php-7
+ * @author             ${JProof}
+ * @copyright          ${copyright}
+ * @license            ${license}
+ * @link
+ * @see                ${docu}
+ * @since              23.09.2017
+ */
+
+declare(strict_types=1);
