2ed4cca9718812f48c8ad982566029ce8dcc0e26
roman JProof <12245927+JProof@users.noreply.github.com>
2017-09-28 22:23:02 +0200

    FileBrowser example base for simple testings. Codestyle

--- examples/demo/Filebrowser/Directories.php
+++ examples/demo/Filebrowser/Directories.php
@@ -44,6 +44,14 @@ namespace Xajax\Examples\Demo\Filebrowser {
 		 * @var string
 		 */
 		protected $docFromDir;
+		/**
+		 * @var string
+		 */
+		protected static $parentNodePrefix = 'parent_';
+		/**
+		 * @var string
+		 */
+		protected static $childNodePrefix = 'child_';
 
 		/**
 		 * @return \RecursiveDirectoryIterator
@@ -51,13 +59,85 @@ namespace Xajax\Examples\Demo\Filebrowser {
 		 */
 		public function getDirectoriesFromDir(): \RecursiveDirectoryIterator
 		{
-			$fdDir = '';
-			if ('' === ($fdDir = $this->getDocFromDir()) && '' === ($fdDir = $this->getDocRoot()))
+
+			if ('' === $fdDir = $this->getDocFromDir())
 			{
-				throw new RuntimeException('Missing of setting the from Dir');
+				if ('' === ($fdDirRoot = $this->getDocRoot()))
+				{
+					throw new RuntimeException('Missing of setting the from Dir');
+				}
+				$fdDir = $fdDirRoot;
+				$this->setDocFromDir($fdDir);
 			}
 
 			return new RecursiveDirectoryIterator($fdDir);
+		}
+
+		/**
+		 * Has Found or not
+		 *
+		 * @param string $nodeHash
+		 * @param bool   $start
+		 *
+		 * @return bool
+		 */
+		public function getRecursiveDir($nodeHash = '', $start = false): bool
+		{
+			// dummy call
+			$this->getDirectoriesFromDir();
+			$changed = false;
+			$nodes   = self::splitPathway($nodeHash);
+
+			foreach ($nodes as $cntr => $node)
+			{
+
+				if ($start)
+				{
+					if ($nodes[0] === self::getHash($this->getDocFromDir()))
+					{
+						$start   = false;
+						$changed = true;
+						continue;
+					}
+				}
+
+				if ($node === $nodeHash)
+				{
+					return true;
+				}
+
+				$dirs = $this->getDirectoriesFromDir();
+				foreach ($dirs as $dir)
+				{
+					$pName = $dir->getPathname();
+					$hash  = self::getHash($pName);
+					if ($hash !== $node)
+					{
+						continue;
+					}
+					if ($changed)
+					{
+						unset($nodes[$cntr - 1]);
+					}
+					else
+					{
+						unset($nodes[$cntr]);
+					}
+					if (0 < count($nodes))
+					{
+						$this->setDocFromDir($dir->getPathname());
+						$this->getRecursiveDir(self::stringifyPathway($nodes));
+
+						return true;
+					}
+					else
+					{
+						return true;
+					}
+				}
+			}
+
+			return false;
 		}
 
 		/**
@@ -118,6 +198,123 @@ namespace Xajax\Examples\Demo\Filebrowser {
 			$this->docFromDir = $docFromDir;
 
 			return $this;
+		}
+
+		public static function getParentNodeForNewChild($parent = ''): string
+		{
+			return self::prefixLastPart($parent, self::getChildNodePrefix());
+		}
+
+		/**
+		 * Simple Helper
+		 *
+		 * @param string $parent
+		 * @param string $prefix
+		 *
+		 * @return string
+		 */
+		public static function prefixLastPart($parent = '', $prefix = '_'): string
+		{
+			$splits = self::splitPathway($parent);
+
+			return $prefix . (string) end($splits);
+		}
+
+		/**
+		 * Simple helper to get NodeName of the <ul> container
+		 *
+		 * @param string $dir
+		 *
+		 * @return string
+		 */
+		public static function getParentNodeHashId($dir = ''): string
+		{
+			return self::getParentNodePrefix() . self::getHash($dir);
+		}
+
+		/**
+		 * Simple helper to get NodeName of the <ul> container
+		 *
+		 * @param string $dir
+		 *
+		 * @return string
+		 */
+		public static function getChildNodeHashId($dir = ''): string
+		{
+			return self::getChildNodePrefix() . self::getHash($dir);
+		}
+
+		/**
+		 * Getting the wayBack
+		 *
+		 * @example  "md5(rootDir)"."md5(subDir)"."md5(subSubDir)"
+		 * @example  9950eb156b8279af6a81c0ed3fc45989.6e657a8541dcdfb47f4c81ce6db2486d
+		 *
+		 * @param string $child
+		 * @param string $parent
+		 *
+		 * @return string
+		 */
+		public static function compilePathway($child = '', $parent = ''): string
+		{
+			$parent = trim($parent);
+			if ('' === $parent)
+			{
+				return $child;
+			}
+			$parentParts   = self::splitPathway($parent);
+			$parentParts[] = $child;
+
+			return implode('.', $parentParts);
+		}
+
+		/**
+		 * @param string $nodes
+		 *
+		 * @return array
+		 */
+		public static function splitPathway($nodes = ''): array
+		{
+			return explode('.', (string) $nodes);
+		}
+
+		/**
+		 * @param array $parts
+		 *
+		 * @return string
+		 */
+		public static function stringifyPathway(array $parts = []): string
+		{
+			return implode('.', $parts);
+		}
+
+		/**
+		 * Navigate through the nodes should be only do with an hash
+		 *
+		 * @param string $dir
+		 *
+		 * @return string
+		 */
+		public static function getHash($dir = ''): string
+		{
+			return basename((string) $dir);
+			//return md5('xajaxSecret' . (string) $dir);
+		}
+
+		/**
+		 * @return string
+		 */
+		public static function getParentNodePrefix(): string
+		{
+			return self::$parentNodePrefix;
+		}
+
+		/**
+		 * @return string
+		 */
+		public static function getChildNodePrefix(): string
+		{
+			return self::$childNodePrefix;
 		}
 	}
 }
--- examples/demo/Filebrowser/index.php
+++ examples/demo/Filebrowser/index.php
@@ -16,12 +16,14 @@ declare(strict_types=1);
 use Xajax\Examples\Demo\Filebrowser\Directories;
 
 require_once dirname(__DIR__) . '/bootstrap.php';
-$xajax = Xajax\Factory::getInstance();
-
+$xajax       = Xajax\Factory::getInstance();
+$xajaxConfig = $xajax->getConfig();
+$xajaxConfig->setUseUncompressedScripts(true);
+$xajaxConfig->setDeferScriptGeneration(false);
 /**
  * Simple instance
  *
- * @return \Xajax\Examples\Demo\Filebrowser\Directories
+ * @return Directories
  */
 function getDirectories()
 {
@@ -39,22 +41,86 @@ function getDirectories()
 /**
  * Method where xajax sends the request against. After Running the Method the objResponse goes back to Xajax an will be send to the browser
  *
- * @param array $params
+ * @param string $params
  *
  * @return \xajaxResponse
  */
 function listDir(string $params = '')
 {
+	$objResponse = xajaxResponse::getInstance();
 
 	$directories = getDirectories();
 
-	$objResponse = xajaxResponse::getInstance();
+	$directories->getRecursiveDir($params, true);
 
-	return $objResponse->alert('selected node ' . $params);
+	$openButton = Directories::prefixLastPart($params, 'open_');
+
+	$tmpl = htmlCreateNode($directories, $params);
+	if ('' === $tmpl)
+	{
+
+		$objResponse->attrReplace($openButton, 'aria-disabled', 'disabled', 'true');
+		$objResponse->assign($openButton, 'className', 'btn badge badge-warning disabled');
+		$objResponse->append($openButton, 'innerHTML', ' There are no directories deeper');
+	}
+	else
+	{
+		// attaching the whole html template
+		$objResponse->assign(Directories::getParentNodeForNewChild($params), 'innerHTML', $tmpl);
+	}
+
+	return $objResponse;
 }
 
-function helperLink()
+function htmlCreateNode(Directories $directories, $parent = '')
 {
+	ob_start();
+	$dirs          = $directories->getDirectoriesFromDir();
+	$simpleExclude = ['.', '..', '.git', '.idea'];
+	$fromDir       = $directories->getDocFromDir();
+
+	if ('' === $parent)
+	{
+		$parent = Directories::getHash($fromDir);
+	}
+	else
+	{
+		$n = Directories::splitPathway($parent);
+		//	array_pop($n);
+		$parent = Directories::stringifyPathway($n);
+	}
+
+	?>
+	<?php
+	foreach ($dirs as $dir)
+	{
+
+		if (!in_array($dir->getFilename(), $simpleExclude, true) && is_dir($dir->getPathname()))
+		{
+
+			$node = Directories::getHash($dir->getPathname());
+
+			?>
+					<li><span><?php echo $node ?></span> - <span><?php echo basename($dir->getPathname()); ?></span>
+						<div class="btn-group-justified">
+							<a role="button" id="<?php echo Directories::prefixLastPart($node, 'open_') ?>" class="btn badge badge-info"
+							   href="javascript:void(null)"
+							   onclick="xajax_listDir('<?php echo Directories::compilePathway($node, $parent) ?>')"><i class="fa fa-plus"></i></a>
+						</div>
+						<div id="<?php echo Directories::getChildNodeHashId($dir->getPathname()) ?>">
+						</div>
+					</li>
+			<?php
+		}
+		?>
+		<?php
+	}
+
+	$content = ob_get_contents();
+	ob_end_clean();
+	$content = trim($content);
+
+	return ('' === $content) ? '' : '<ul id="' . Directories::getParentNodeHashId($fromDir) . '">' . $content . '</ul>';
 }
 
 $xajax->getConfig()->setJavascriptURI('../../../');
@@ -70,35 +136,24 @@ $directories = getDirectories();
 	<head>
 		<meta charset="UTF-8">
 		<title><?php echo basename(__DIR__) ?></title>
-	    <?php $xajax->printJavascript(); ?>
+		<link rel="stylesheet" href="../assets/font-awesome-4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
+			  integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 	</head>
 	<body>
-    <?php
-
-    $dirs          = $directories->getDirectoriesFromDir();
-    $simpleExclude = ['.', '..', '.git', '.idea'];
-    ?>
-	<ul><?php
-
-	    foreach ($dirs as $dir)
-	    {
-
-		    if (!in_array($dir->getFilename(), $simpleExclude, true) && is_dir($dir->getPathname()))
-		    {
-			    $node = md5($dir->getPathname())
-			    ?>
-						<li id="main_<?php echo $node ?>"><a href="javascript:void(null)"
-															 onclick="xajax_listDir('<?php echo $node ?>')">Sub</a>
-				<?php echo $dir->getPathname(); ?>
-							<div id="sub_<?php echo $node ?>"></div>
-						</li>
-			    <?php
-		    }
-	    }
-
+	<div class="container-fluid">
+	    <?php
+	    echo htmlCreateNode($directories);
 	    ?>
-	</ul>
-	<a href="javascript:void(null)" onclick="<?php echo $xajaxFunctionListDir->getScript() ?>">list</a>
+		<a href="javascript:void(null)" onclick="<?php echo $xajaxFunctionListDir->getScript() ?>">list</a>
+	</div>
+    <?php $xajax->printJavascript(); ?>
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+			crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
+			integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
+			integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 	</body>
 	</html>
 <?php
